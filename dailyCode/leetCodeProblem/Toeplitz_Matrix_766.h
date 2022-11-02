835bc80d7a772332f288109fe66a15b6cb46fe4d
henongj <henongj@DESKTOP-RH38KPU>
2022-11-02 19:21:34 +0900

    2022-11-02 중간저장

---
+++ dailyCode/leetCodeProblem/Toeplitz_Matrix_766.h
@@ -0,0 +1,19 @@
+#ifndef ___Toeplitz_Matrix_h___
+#define ___Toeplitz_Matrix_h___
+
+#include<iostream>
+#incldue<string>
+using namespace std;
+class Solution {
+public:
+    bool isInside(const vector<vector<int>> )
+    {
+        
+    }
+
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        
+    }
+};
+
+#endif
