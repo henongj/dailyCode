66f1c83b88785423e1d7d8cf70de38dd199d21a8
henongj <henongj@DESKTOP-RH38KPU>
2022-11-06 01:14:17 +0900

    191, solved simple bit count problem

---
+++ dailyCode/leetCodeProblem/Number_of_1_Bits.h
@@ -0,0 +1,25 @@
+
+#ifndef ___Number_of_1_Bits_h___
+#define ___Number_of_1_Bits_h___
+/*
+	191. Number of 1 Bits
+	https://leetcode.com/problems/number-of-1-bits/
+
+*/
+class Solution {
+public:
+	int hammingWeight(uint32_t n)
+	{
+		int count = 0;
+		while (n != 0)
+		{
+			if ((n & 1) == 1)
+			{
+				count++;
+			}
+			n = n >> 1;
+		}
+		return count;
+	}
+};
+#endif
