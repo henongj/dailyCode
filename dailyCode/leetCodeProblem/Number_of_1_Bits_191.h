837b1e5defaafae67df1f3f96ea7709159c4d586
henongj <henongj@DESKTOP-RH38KPU>
2022-11-06 01:32:26 +0900

    simple bit counting problem

--- dailyCode/leetCodeProblem/Number_of_1_Bits_191.h
+++ dailyCode/leetCodeProblem/Number_of_1_Bits_191.h
@@ -1,6 +1,6 @@
 
-#ifndef ___Number_of_1_Bits_h___
-#define ___Number_of_1_Bits_h___
+#ifndef ___Number_of_1_Bits_191_h___
+#define ___Number_of_1_Bits_191_h___
 /*
 	191. Number of 1 Bits
 	https://leetcode.com/problems/number-of-1-bits/
