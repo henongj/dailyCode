a18023a98905f2f2950c9208e4d0a82392409829
henongj <henongj@DESKTOP-RH38KPU>
2022-11-07 21:04:32 +0900

    problems

--- dailyCode/leetCodeSubmitMain.cpp
+++ dailyCode/leetCodeSubmitMain.cpp
@@ -6,49 +6,25 @@
 #include<queue>
 using namespace std;
 
-class Solution {
+// https://leetcode.com/problems/find-center-of-star-graph/
+// 1791  Find Center of Star Graph
+class Solution{
 public:
-	bool find_path_dfs(vector<vector<int>>& adjacent_list, vector<vector<bool>>& visited, int source, int destination)
-	{
-		if (source == destination)
-			return true;
-
-		visited[source][destination] = true;
-		for (auto node : adjacent_list[source])
-		{
-			if (!visited[source][node])
-			{
-				if (find_path_dfs(adjacent_list, visited, node, destination))
-					return true;
-			}
-		}
-		return false;
-	}
-	bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-		vector<vector<int>> adjacent_list;
-		vector<vector<bool>> visited;
-		for (int i = 0; i < n; i++)
-		{
-			vector<int> v;
-			vector<bool> b;
-			for (int i = 0; i < n; i++)
-			{
-				v.push_back(-1);
-				b.push_back(false);
-			}
-			adjacent_list.push_back(v);
-			visited.push_back(b);
-		}
+    int findCenter(vector<vector<int>>& edges) {
+		int num_of_edges = edges.size();
 		
-		for (auto edge : edges)
-		{
-			adjacent_list[ edge[0] ].push_back(edge[1]);
-			adjacent_list[ edge[1] ].push_back(edge[0]);
-		}
-		return find_path_dfs(adjacent_list,visited,source,destination);
-	}
+    }
 };
-	
+
+//997. find the town judge
+//https://leetcode.com/problems/find-the-town-judge/
+class Solution{
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+
+    }
+};
+//797
 int main(void)
 {
 	test_depth_limited_search();
