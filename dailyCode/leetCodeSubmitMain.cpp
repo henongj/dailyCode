1e932172bdab4ffdc63cd8314bd713865e7da414
henongj <henongj@DESKTOP-RH38KPU>
2022-11-07 20:54:35 +0900

    incorrect answer, incomplete

--- dailyCode/leetCodeSubmitMain.cpp
+++ dailyCode/leetCodeSubmitMain.cpp
@@ -4,43 +4,48 @@
 #include<string>
 #include<algorithm>
 #include<queue>
-#include"dataStruct/graph_traversal.h"
 using namespace std;
-
-vector<vector<bool>> get_matrix(int n)
-{
-	vector<vector<bool>> v;
-	for (int i = 0; i < n; i++)
-	{
-		vector<bool> v1;
-		for (int j = 0; j < n; j++)
-		{
-			v1.push_back(false);
-		}
-		v.push_back(v1);
-	}
-	return v;
-}
 
 class Solution {
 public:
-	
+	bool find_path_dfs(vector<vector<int>>& adjacent_list, vector<vector<bool>>& visited, int source, int destination)
+	{
+		if (source == destination)
+			return true;
 
-	
+		visited[source][destination] = true;
+		for (auto node : adjacent_list[source])
+		{
+			if (!visited[source][node])
+			{
+				if (find_path_dfs(adjacent_list, visited, node, destination))
+					return true;
+			}
+		}
+		return false;
+	}
 	bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-
-		vector<vector<int>> adjacent_list(-1);
-		auto visited = get_matrix(n);
+		vector<vector<int>> adjacent_list;
+		vector<vector<bool>> visited;
+		for (int i = 0; i < n; i++)
+		{
+			vector<int> v;
+			vector<bool> b;
+			for (int i = 0; i < n; i++)
+			{
+				v.push_back(-1);
+				b.push_back(false);
+			}
+			adjacent_list.push_back(v);
+			visited.push_back(b);
+		}
 		
 		for (auto edge : edges)
 		{
 			adjacent_list[ edge[0] ].push_back(edge[1]);
 			adjacent_list[ edge[1] ].push_back(edge[0]);
 		}
-		
-		
-		
-		
+		return find_path_dfs(adjacent_list,visited,source,destination);
 	}
 };
 	
