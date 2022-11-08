d620976275d750e77bd632197312b4121250cb11
henongj <idjimj1@naver.com>
2022-11-08 19:21:39 +0900

    1971, basic graph problem, using matrix,

--- dailyCode/leetCodeProblem/Find_if_Path_Exists_in_Graph_1971.h
+++ dailyCode/leetCodeProblem/Find_if_Path_Exists_in_Graph_1971.h
@@ -6,44 +6,39 @@
 
 class Solution {
 public:
-	bool find_path_dfs(vector<vector<int>>& adjacent_list, vector<vector<bool>>& visited, int source, int destination)
-	{
-		if (source == destination)
-			return true;
+	
+	bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+		vector<int> neighbors[n];
+		for (auto edge : edges) { // ¿Œ¡¢
+			neighbors[edge[0]].push_back(edge[1]);
+			neighbors[edge[1]].push_back(edge[0]);
+		}
+		vector<bool> visited(n, false);
+		
+		queue<int> q;
+		q.push(source);
+		visited[source] = true;
 
-		visited[source][destination] = true;
-		for (auto node : adjacent_list[source])
+		while(!q.empty())
 		{
-			if (!visited[source][node])
+			int target = q.front();
+			q.pop();
+			
+			if (target == destination)
+				return true;
+			else
 			{
-				if (find_path_dfs(adjacent_list, visited, node, destination))
-					return true;
+				for (auto adjacent : neighbors[target])
+				{
+					if (!visited[adjacent)
+					{
+						q.push(adjacent);
+						visited[adjacent] = true;
+					}
+				}
 			}
 		}
 		return false;
-	}
-	bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-		vector<vector<int>> adjacent_list;
-		vector<vector<bool>> visited;
-		for (int i = 0; i < n; i++)
-		{
-			vector<int> v;
-			vector<bool> b;
-			for (int i = 0; i < n; i++)
-			{
-				v.push_back(-1);
-				b.push_back(false);
-			}
-			adjacent_list.push_back(v);
-			visited.push_back(b);
-		}
-
-		for (auto edge : edges)
-		{
-			adjacent_list[edge[0]].push_back(edge[1]);
-			adjacent_list[edge[1]].push_back(edge[0]);
-		}
-		return find_path_dfs(adjacent_list, visited, source, destination);
 	}
 };
 
