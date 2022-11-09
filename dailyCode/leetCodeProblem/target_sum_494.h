2d322ca02a84fa9bad5633730d38020e2e4a924f
henongj <henongj@DESKTOP-RH38KPU>
2022-11-09 21:07:17 +0900

    add refactoring solution, more fase

--- dailyCode/leetCodeProblem/target_sum_494.h
+++ dailyCode/leetCodeProblem/target_sum_494.h
@@ -1,3 +1,5 @@
+#ifndef ___target_sum_494_h___
+#define ___target_sum_494_h___
 //https://leetcode.com/problems/target-sum/
 //___target_sum_494_h___
 #include<vector>
@@ -6,7 +8,7 @@ using namespace std;
 class Solution {
 public:
 	void helper(const vector<int>& nums, int target, int sum, int& count, int index)
-	{
+	{//Runtime: 2794 ms , Memory Usage : 9.1 MB
 		if (index == nums.size() && sum == target)
 			count += 1;
 		else if (index < nums.size())
@@ -16,6 +18,24 @@ public:
 		}
 		return;
 	}
+	void helper2(const vector<int>& nums, int target, int sum, int& count, int index)
+	{
+		if (index == nums.size() && sum == target)
+			count += 1;
+
+		if (index < nums.size())
+		{
+			helper(nums, target, sum + nums[index], count, index + 1);
+			helper(nums, target, sum - nums[index], count, index + 1);
+		}
+		return;
+
+
+		/*
+		Runtime: 1760 ms, faster than 14.30% of C++ online submissions for Target Sum.
+		Memory Usage: 8.9 MB, less than 93.73% of C++ online submissions for Target Sum.
+		*/
+	}
 	int findTargetSumWays(vector<int>& nums, int target) {
 
 		int count = 0;
@@ -24,3 +44,5 @@ public:
 		return count;
 	}
 };
+
+#endif
