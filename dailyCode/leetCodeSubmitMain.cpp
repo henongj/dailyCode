f5d0367cd6adf1766d0e48d9d5e73ecccd605749
henongj <idjimj1@naver.com>
2022-11-09 17:47:34 +0900

    997 problem, incomplete, temporary save

--- dailyCode/leetCodeSubmitMain.cpp
+++ dailyCode/leetCodeSubmitMain.cpp
@@ -4,30 +4,39 @@
 #include<string>
 #include<algorithm>
 #include<queue>
+#include<unordered_map>
+#include<map>
 using namespace std;
 
-// https://leetcode.com/problems/find-center-of-star-graph/
-// 1791  Find Center of Star Graph
-class Solution{
-public:
-    int findCenter(vector<vector<int>>& edges) {
-		int num_of_edges = edges.size();
-		
-    }
-};
 
 //997. find the town judge
 //https://leetcode.com/problems/find-the-town-judge/
 class Solution{
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
 
+    int findJudge(int n, vector<vector<int>>& trust) {
+		
+		int judge = -1;
+		
+		unordered_map<int, int> trust_mapping;
+		
+		for (auto i : trust) 
+			trust_mapping[i[0]] = i[1];
+
+		for (int i = 0; i < trust_mapping.size(); i++)
+		{ // 대상이 믿는 사람이 없다면 trust_mapping에도 없다
+			if (!trust_mapping.count(trust_mapping[i])) // 믿는 대상이 없을 경우
+				return trust_mapping[i];
+		}
+		
+		return judge;
     }
 };
 //797
+
+
 int main(void)
 {
-
 	
 	return 0;
 }
