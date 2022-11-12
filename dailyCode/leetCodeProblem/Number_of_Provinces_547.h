1f9be39f5cae3c0809b83dce22c2c49b4ba31794
henongj <idjimj1@naver.com>
2022-11-13 01:57:46 +0900

    Number_of_Provinces_547, adjacent_array or list

--- dailyCode/leetCodeProblem/Number_of_Provinces_547.h
+++ dailyCode/leetCodeProblem/Number_of_Provinces_547.h
@@ -1,27 +1,40 @@
-#include<iostream>
-#include<vector>
-#include<utility>
-#include<string>
-#include<algorithm>
-#include<queue>
-#include<unordered_map>
-#include<map>
-using namespace std;
-
-
 // 547 ___Number_of_Provinces_547_h___
 #ifndef ___Number_of_Provinces_547_h___
 #define ___Number_of_Provinces_547_h___
 // https://leetcode.com/problems/number-of-provinces/
 #include<vector>
 #include<iostream>
+#include<queue>
 using namespace std;
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
+	int findCircleNum(vector<vector<int>>& isConnected) {
 		vector<bool> visited(isConnected.size(), false);
 		queue<int> q;
-		
+		int count = 0;
+		for (int i = 0; i < isConnected.size(); i++) {
+			if (!visited[i])
+			{
+				q.push(i);
+				visited[i] = true;
+				while (!q.empty()) {
+					int node = q.front();
+					q.pop();
+					for (int next = 0; next < isConnected.size(); next++) {
+						if (isConnected[node][next] == 1 && !visited[next]) 
+						{
+							q.push(next);
+							visited[next] = true;
+						}
+					}
+				}
+				count++;
+			}
+		}
+	}
+	int findCircleNum2(vector<vector<int>>& isConnected) {
+		vector<bool> visited(isConnected.size(), false);
+		queue<int> q;
 		int count = 0;
 		for (int i = 0; i < isConnected.size(); i++) {
 			if (!visited[i])
@@ -41,39 +54,8 @@ public:
 				count++;
 			}
 		}
-    }
-
-	int findCircleNum2(vector<vector<int>>& isConnected) {
-		vector<bool> visited(isConnected.size(), false);
-		queue<int> q;
-		int count = 0;
-		for (int i = 0; i < isConnected.size(); i++) {
-			if (!visited[i])
-			{
-			q.push(i);
-				visited[i] = true;
-				while (!q.empty()) {
-				int node = q.front();
-					q.pop();
-					for (int next = 0; next < isConnected.size(); next++) {
-						if (isConnected[node][next] == 1 && !visited[next]) {
-							q.push(next);
-						visited[next] = true;
-						}
-					}
-				}
-				count++;
-			}
-		}
 		return count;
 	}
 };
 
-
 #endif
-
-int main(void)
-{
-	
-	return 0;
-}
