949cde21c012d3aadd4eb3c736cd53f0f542308e
henongj <idjimj1@naver.com>
2022-11-14 16:29:01 +0900

    temo

--- dailyCode/leetCodeSubmitMain.cpp
+++ dailyCode/leetCodeSubmitMain.cpp
@@ -10,44 +10,21 @@ using namespace std;
 
 
 /*
-1615. 
-___Maximal_Network_Rank_1615_h___
-https://leetcode.com/problems/maximal-network-rank
+// leetcide 974 ___Most_Stones_Removed_with_Same_Row_or_Column_947_h___
+https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
 */
+#ifndef ___Most_Stones_Removed_with_Same_Row_or_Column_947_h___
+#define ___Most_Stones_Removed_with_Same_Row_or_Column_947_h___
+
 class Solution {
 public:
-    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-		
-		vector<vector<int>> adj(n, vector<int>(n,0));
-		vector<int> edge_count(n, 0);
-		
-		int result = 0;
-
-		for (int road = 0; road < roads.size(); road++)
-		{
-			//edge를 각 노드에 연결
-			edge_count[roads[road][0]] += 1;
-			edge_count[roads[road][1]] += 1;
-			adj[roads[road][0]][roads[road][1]] = 1;
-			adj[roads[road][1]][roads[road][0]] = 1;
-			
-		}
-
-		for (int node1 = 0; node1 < n; node1++)
-		{
-			for (int node2 = node1 + 1; node2 < n; node2++)
-			{
-				int sum = edge_count[node1] + edge_count[node2];
-				if (adj[node1][node2] == 1) // 서로 연결되어 있으면
-					sum = sum - 1;
-				result = max(result, sum);
-			}
-		}
-
-		return result;
-		
+    int removeStones(vector<vector<int>>& stones) {
+		vector<vector<int>> graph = 
     }
 };
+
+#endif
+
 
 
 int main(void)
