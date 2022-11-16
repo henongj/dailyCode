1c02835cb9df926dc061b21d921b60f8eed118d2
henongj <henongj@DESKTOP-RH38KPU>
2022-11-16 19:52:54 +0900

    make max heap

---
+++ dailyCode/dataStruct/max_heap.h
@@ -0,0 +1,65 @@
+#ifndef ___max_heap_h___
+#define ___max_heap_h___
+#include<vector>
+#include<iostream>
+using namespace std;
+class MaxHeap
+{
+private:
+	vector<int> arr;
+	void heapify(int idx)
+	{
+		int left = 2 * idx + 1;
+		int right = 2 * idx + 2;
+		int parent = idx;
+		if (left < arr.size() && arr[left] > arr[parent])
+		{
+			swap(left, parent);
+			heapify(left);
+		}
+		if (right < arr.size() && arr[right] > arr[parent])
+		{
+			swap(right, parent);
+			heapify(right);
+		}
+	}
+	void swap(int idx1, int idx2)
+	{
+		int t = arr[idx1];
+		arr[idx1] = arr[idx2];
+		arr[idx2] = t;
+	}
+public:
+	MaxHeap(vector<int> arr) {
+		this->arr = arr;
+		for (int i = arr.size() / 2 - 1; i >= 0; i--)
+			heapify(i);
+	}
+	int size(void)
+	{
+		return arr.size();
+	}
+	int front(void)
+	{
+		return arr[0];
+	}
+	void pop(void)
+	{
+		swap(0, arr.size() - 1);
+		arr.pop_back();
+		heapify(0);
+	}
+	void push(int num)
+	{
+		arr.push_back(num);
+		swap(0, arr.size() - 1);
+		heapify(0);
+	}
+	void print()
+	{
+		for (int i = 0; i < arr.size(); i++)
+			cout << arr[i] << " ";
+		cout << endl;
+	}
+};
+#endif
