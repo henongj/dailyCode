d18322bf19ac9b3692bd9bb1a3923cf1c12f4de6
henongj <henongj@DESKTOP-RH38KPU>
2022-11-16 21:13:01 +0900

    빈도가 중복이면 사전순으로 정렬해야 정답이 된다

--- dailyCode/leetCodeSubmitMain.cpp
+++ dailyCode/leetCodeSubmitMain.cpp
@@ -8,24 +8,103 @@
 #include<map>
 using namespace std;
 
-
-/*
-// leetcide 974 ___Most_Stones_Removed_with_Same_Row_or_Column_947_h___
-https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
-*/
-#ifndef ___Most_Stones_Removed_with_Same_Row_or_Column_947_h___
-#define ___Most_Stones_Removed_with_Same_Row_or_Column_947_h___
+// priority queue
+class MaxHeap
+{
+private:
+	// str , frequency
+	vector<pair<string,int>> arr;
+	void heapify(int idx)
+	{
+		int left = 2 * idx + 1;
+		int right = 2 * idx + 2;
+		int parent = idx;
+		if (left < arr.size() && arr[left].second > arr[parent].second)
+		{
+			swap(left, parent);
+			heapify(left);
+		}
+		if (right < arr.size() && arr[right].second > arr[parent].second)
+		{
+			swap(right, parent);
+			heapify(right);
+		}
+	}
+	void swap(int idx1, int idx2)
+	{
+		pair<string,int> t = arr[idx1];
+		arr[idx1] = arr[idx2];
+		arr[idx2] = t;
+	}
+public:
+	MaxHeap(vector<pair<string,int>> arr) {
+		this->arr = arr;
+		for (int i = arr.size() / 2 - 1; i >= 0; i--)
+			heapify(i);
+	}
+	int size(void)
+	{
+		return arr.size();
+	}
+	pair<string,int> front(void)
+	{
+		return arr[0];
+	}
+	void pop(void)
+	{
+		swap(0, arr.size() - 1);
+		arr.pop_back();
+		heapify(0);
+	}
+	void push(pair<string,int> num)
+	{
+		arr.push_back(num);
+		swap(0, arr.size() - 1);
+		heapify(0);
+	}
+	void print()
+	{
+		for (int i = 0; i < arr.size(); i++)
+			cout << arr[i].first << " " << arr[i].second << " ";
+		cout << endl;
+	}
+};
+//https://leetcode.com/problems/top-k-frequent-words/
+//692. ___Top_K_Frequent_Words_692_h___
+#ifndef ___Top_K_Frequent_Words_692_h___
+#define ___Top_K_Frequent_Words_692_h___
 class Solution {
 public:
-    int removeStones(vector<vector<int>>& stones) {
+	vector<string> topKFrequent(vector<string>& words, int k) {
+		vector<string> result;
+		vector<pair<string, int>> converter;
+		unordered_map<string, int> m;
+		for (auto word : words)
+		{
+			if (m.count(word))
+				m[word] = m[word] + 1;
+			if (!m.count(word))
+				m[word] = 1;
+		}
 		
+		for (auto data : m)
+			converter.push_back(pair<string, int>(data.first, data.second));
+
+		MaxHeap heap = MaxHeap(converter);
+
+		// 사전순으로 정렬해야함
+		for (int i = 0; i < k; i++)
+		{
+			result.push_back(heap.front().first);
+			heap.pop();
+		}
+		return result;
+	}
+
 };
-
 #endif
-
 
 int main(void)
 {
-	
 	return 0;
 }
