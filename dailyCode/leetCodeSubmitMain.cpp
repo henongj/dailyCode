46f3d69ec2b2bc80853bafcabcb4feaebd320b5c
henongj <idjimj1@naver.com>
2022-11-26 23:02:57 +0900

    empty main

--- dailyCode/leetCodeSubmitMain.cpp
+++ dailyCode/leetCodeSubmitMain.cpp
@@ -11,11 +11,7 @@ using namespace std;
 
 int main(void)
 {
-	vector<int> ingredient1 = { 2, 1, 1, 2, 3, 1, 2, 3, 1 };
-	vector<int> ingredient2 = { 1, 3, 2, 1, 2, 1, 3, 1, 2 };
-	cout << "answer : " << solution(ingredient1) << endl;
-	char c;
-	cin >> c;
+	
 	return 0;
 }
 
