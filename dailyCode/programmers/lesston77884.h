6573df9408214e8dfd247a5223f7b28da7928269
henongj <idjimj1@naver.com>
2022-11-26 23:48:51 +0900

    약수의 개수와 덧셈 lv1

---
+++ dailyCode/programmers/lesston77884.h
@@ -0,0 +1,33 @@
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/77884
+//¾à¼öÀÇ °³¼ö¿Í µ¡¼À lv1
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> getDivisor(int num)
+{
+	vector<int> divisor;
+	for (int i = 1; i <= num; i++)
+	{
+		if (num % i == 0)
+			divisor.push_back(i);
+	}
+	return divisor;
+}
+
+int solution(int left, int right) {
+	int answer = 0;
+
+	for (int i = left; i <= right; i++)
+	{
+		vector<int> divisor = getDivisor(i);
+		if (divisor.size() % 2 == 0)
+			answer += i;
+		else
+			answer -= i;
+	}
+
+	return answer;
+}
