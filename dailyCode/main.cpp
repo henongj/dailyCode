967d18ee9f4430ea917d1e1b27670013e498a50c
henongj <idjimj1@naver.com>
2022-12-12 21:36:54 +0900

    메인을 구분할 이유가 없어보임

---
+++ dailyCode/main.cpp
@@ -0,0 +1,19 @@
+#include<algorithm>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<utility>
+#include<vector>
+#include<sstream>
+#include<stack>
+using namespace std;
+
+
+int main(void)
+{
+
+	return 0;
+}
+
