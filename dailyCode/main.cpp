e1f88826f251f6c5d346cd6fe755abdc91af94f0
henongj <idjimj1@naver.com>
2022-12-26 17:08:58 +0900

    acmicpc 14890 중간저장

--- dailyCode/main.cpp
+++ dailyCode/main.cpp
@@ -43,34 +43,136 @@ void solution(void)
 {
 	int N, L;
 	vector<vector<int>> map;
-	input_data(N,L,map);
+	input_data(N, L, map);
 	int answer = 0;
-
 	int count = 1;
 	bool flag = true;
+
+	// make lamda function to get diff value, return int 
+	auto diff = [](int a, int b) -> int { return b - a; };
+	// make lamda function reset count and flag
+	auto reset = [&count, &flag]() -> void { count = 1; flag = true; };
+	
 	for (int row = 0; row < map.size(); row++)
 	{
-		for (int col = 0; col < map[0].size(); col++)
+		int len = map[row].size();
+		for (int col = 0; col < len - 1; col++)
 		{
-			
-			// 1. 현재 위치의 높이가 다음 위치의 높이와 같다면 count++ 다음 위치로 이동
-			if (map[row][col] == map[row][col + 1])
+			int gap = diff(map[row][col], map[row][col + 1]);
+
+			// 1. 현재 위치의 높이가 다음 위치의 높이와 같다면 count++
+			if (gap == 0)
+			{
 				count++;
-			// 2. 현재 위치의 높이가 다음 위치의 높이보다 1 높고 count >= L 이면 count = 1 다음 위치로 이동
-			if (map[row][col] == map[row][col + 1] + 1 && count >= L)
+				// 3.1 내리막 경사로 설치 가능한 경우 설치
+				if (flag == false && count >= L)
+					reset();
+			}
+			// 2. 오르막이면서
+			else if (gap == 1)
+			{
+				// 경사로 길이가 충분 X
+				if (count < L)
+					break;
+				// 충분 O
 				count = 1;
-			// 3. 현재 위치의 높이가 다음 위치의 높이보다 1 낮으면 count = 1 다음 위치로 이동
-			// 3.1 count를 L만큼 쌓지 못하면 break
+			}
+			// 3. 내리막이면서 
+			else if (gap == -1)
+			{
+				//막 내려왔으면 false로 표시
+				if (flag)
+					reset();
+				// 3.2 이미 내려왔는데
+				if (!flag)
+				{
+					// 경사로 길이가 충분 X
+					if (count < L)
+						break;
+					// 충분 O
+					count = 1;
+				}
+			}
+			// 4. 높이차가 2이상이면 경사로 설치 불가
+			else
+				break;
+
+			//
+			if (col == len - 2)
+			{
+				if ((flag == false && count >= L) || flag == true)
+				{
+					reset();
+					answer++;
+				}
+			}
 		}
 	}
 
-	
+	reset();
+	for (int col = 0; col < map.size(); col++)
+	{
+		int len = map[col].size();
+		for (int row = 0; row < len - 1; row++)
+		{
+			int gap = diff(map[row][col], map[row + 1][col]);
+
+			// 1. 현재 위치의 높이가 다음 위치의 높이와 같다면 count++
+			if (gap == 0)
+			{
+				count++;
+				// 3.1 내리막 경사로 설치 가능한 경우 설치
+				if (flag == false && count >= L)
+					reset();
+			}
+			// 2. 오르막이면서
+			else if (gap == 1)
+			{
+				// 경사로 길이가 충분 X
+				if (count < L)
+					break;
+				// 충분 O
+				count = 1;
+			}
+			// 3. 내리막이면서 
+			else if (gap == -1)
+			{
+				//막 내려왔으면 false로 표시
+				if (flag)
+					reset();
+				// 3.2 이미 내려왔는데
+				if(!flag)
+				{
+					// 경사로 길이가 충분 X
+					if (count < L)
+						break;
+					// 충분 O
+					count = 1;
+				}
+			}
+			// 4. 높이차가 2이상이면 경사로 설치 불가
+			else
+				break;
+
+			//
+			if (row == len - 2)
+			{
+				if ((flag == false && count >= L) || flag == true)
+				{
+					reset();
+					answer++;
+				}
+			}
+		}
+	}
+
+	cout << answer << endl;
 }
 #endif // ___problem_14890_h___
 
 int main(void)
 {
-
+	solution();
 	return 0;
 }
 
