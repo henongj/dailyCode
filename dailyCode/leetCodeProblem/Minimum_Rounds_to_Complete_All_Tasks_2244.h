9df53f70fd0ba4b08a1ae9f14f2b2f777cf3ccd4
henongj <henongj@DESKTOP-RH38KPU>
2023-01-04 21:43:40 +0900

    daily leetcode

--- dailyCode/leetCodeProblem/Minimum_Rounds_to_Complete_All_Tasks_2244.h
+++ dailyCode/leetCodeProblem/Minimum_Rounds_to_Complete_All_Tasks_2244.h
@@ -17,24 +17,24 @@
 using namespace std;
 class Solution {
 public:
-	int minimumRounds(vector<int>& tasks) {
+    int minimumRounds(vector<int>& tasks) {
 		int n = tasks.size();
 		int max_len = 100001;
 		int round = 0;
 		unordered_map <int, int> task_count; // task_count[task] = count
-
+		
 		for (int i = 0; i < n; i++)
 			task_count[tasks[i]]++;
-
-		vector<int> memo(max_len, 0);
+		
+		vector<int> memo(max_len,0);
 		memo[1] = -1; memo[2] = 1; memo[3] = 1; memo[4] = 2;
-
+		
 		for (int i = 5; i < max_len; i++)
 			memo[i] = min(memo[i - 2], memo[i - 3]) + 1;
-
+		
 		for (auto i : task_count)
 		{
-			cout << i.first << " " << i.second << endl;
+			//cout << i.first << " " << i.second << endl;
 			int r = memo[i.second];
 			if (r == -1)
 				return -1;
@@ -42,7 +42,7 @@ public:
 		}
 
 		return round;
-
-	}
+		
+    }
 };
 #endif //___Minimum_Rounds_to_Complete_All_Tasks_2244_h___
