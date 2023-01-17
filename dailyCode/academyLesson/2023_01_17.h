fccdddda78ff80a70573cb56c9e137737ae7ee4a
henongj <idjimj1@naver.com>
2023-01-17 14:59:31 +0900

    academy lecture_2023_01_17

---
+++ dailyCode/academyLesson/2023_01_17.h
@@ -0,0 +1,28 @@
+
+void remind(void)
+{
+	int nData1{};
+	int nData2{};
+	int nData3{};
+
+	nData2 = 20;
+	nData3 = 30;
+
+	nData1 = nData2 + nData3;
+
+	cout << nData1;
+}
+
+void firstProgramToGetRectangleArea(void)
+{
+	int nRectangleWdith{};
+	int nRectangleHeight{};
+	int nRectangleArea{};
+
+	nRectangleWdith = 10;
+	nRectangleHeight = 20;
+
+	nRectangleArea = nRectangleWdith * nRectangleHeight;
+
+	std::cout << nRectangleArea << std::endl;
+}
