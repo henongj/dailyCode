f3c420a50690c1947894ae68ec3d2ef5679c12c7
henongj <idjimj1@naver.com>
2023-01-19 15:51:49 +0900

    2023 01 19 lecture commit

--- dailyCode/academyLesson/2023_01_19.h
+++ dailyCode/academyLesson/2023_01_19.h
@@ -1,6 +1,8 @@
 #include<iostream>
 namespace lecture_2023_01_19
 {
+	//캐스팅 재확인
+	// 조건문
 	void remind(void)
 	{
 		int nDestination{};
@@ -34,4 +36,56 @@ namespace lecture_2023_01_19
 
 	}
 	
+	void conditionTest(void)
+	{
+		int nData{};
+
+		scanf_s("%d", &nData);
+
+		if (nData % 2 != 0)
+		{
+			printf("홀수임 \n");
+		}
+
+		if (nData % 2 == 0)
+		{
+			printf("짝수임 \n");
+		}
+
+		if (0 < nData)
+		{
+			printf("양수임 \n");
+		}
+
+		if (nData < 0)
+		{
+			printf("음수임 \n");
+		}
+
+		if (10 <= nData)
+		{
+			printf("두 자리 이상인 수임 \n");
+		}
+
+		if (nData <= 999)
+		{
+			printf("1000보다 작네\n");
+		}
+
+
+	}
+	void conditionTest2(void)
+	{
+		int nData1{};
+		scanf_s("%d", &nData1);
+
+		if (nData1 == 100)
+		{
+			printf("참\n");
+		}
+		if (nData1 == 0)
+		{
+			printf("nData1 = 0하면 nData1이 0이 되서 무조건 거짓임\n");
+		}
+	}
 }
