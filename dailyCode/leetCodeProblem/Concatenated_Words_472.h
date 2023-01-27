edbd93601443abb87921fe8529aa7e611e0ee211
henongj <idjimj1@naver.com>
2023-01-27 13:58:12 +0900

    daily leetcode problem. copilot answer

---
+++ dailyCode/leetCodeProblem/Concatenated_Words_472.h
@@ -0,0 +1,43 @@
+#include<algorithm>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<utility>
+#include<vector>
+#include<sstream>
+#include<stack>
+#include<cmath>	
+#include<set>
+#include<unordered_set>
+using namespace std;
+//https://leetcode.com/problems/concatenated-words/
+//472. ___Concatenated_Words_h___
+class Solution {
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_set<string> words_set;
+        for (string word : words) words_set.insert(word);
+        vector<string> res;
+
+        for (string word : words) {
+            int n = word.size();
+            vector<int> dp(n + 1, 0);
+            dp[0] = 1;
+            for (int i = 0; i < n; i++) {
+                if (!dp[i]) continue;
+                for (int j = i + 1; j <= n; j++) {
+                    if (j - i < n && words_set.count(word.substr(i, j - i))) {
+                        dp[j] = 1;
+                    }
+                }
+                if (dp[n]) {
+                    res.push_back(word);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
