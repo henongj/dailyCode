719cbefe6be1dc98d5bb6d7c0c19b5a802b3091a
henongj <idjimj1@naver.com>
2023-01-28 20:48:53 +0900

    merge

---
+++ dailyCode/acmicpc/acmicpc_18108.h
@@ -0,0 +1,29 @@
+#include<algorithm>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<utility>
+#include<vector>
+#include<sstream>
+#include<stack>
+#include<cmath>
+#include<set>
+using namespace std;
+
+//https://www.acmicpc.net/problem/18108
+//1998년생인 내가 태국에서는 2541년생?!
+void getTailandAge(void)
+{
+	int year;
+	cin >> year;
+	cout << year - 543;
+}
+
+/*int main(void)
+{
+	getTailandAge();
+
+	return 0;
+}*/
