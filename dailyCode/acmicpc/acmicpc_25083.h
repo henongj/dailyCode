d10c2167e47ba60585d8d50c6d955314e8b6fe07
henongj <idjimj1@naver.com>
2023-01-29 16:17:55 +0900

    daily leetcode and 백준 단계별로 풀어보기 1단게 14번

---
+++ dailyCode/acmicpc/acmicpc_25083.h
@@ -0,0 +1,36 @@
+#include<algorithm>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<utility>
+#include<vector>
+#include<sstream>
+#include<stack>
+#include<cmath>
+#include<set>
+using namespace std;
+
+//https://www.acmicpc.net/problem/25083
+//»õ½Ï
+
+
+void fPrintSprout(void)
+{
+    puts("         ,r'\"7");
+    puts("r`-_   ,'  ,/");
+    puts(" \\. \". L_r'");
+    puts("   `~\\/");
+    puts("      |");
+    puts("      |");
+}
+
+
+
+/*int main(void)
+{
+    fPrintSprout();
+
+    return 0;
+}*/
