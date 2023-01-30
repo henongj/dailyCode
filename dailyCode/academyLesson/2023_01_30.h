0d6167ef94f60939ffc289ae23300868c60a1636
henongj <idjimj1@naver.com>
2023-01-30 15:31:45 +0900

    2023_01_30 academy lecture, 반복문

---
+++ dailyCode/academyLesson/2023_01_30.h
@@ -0,0 +1,61 @@
+#include<iostream>
+
+namespace lecture_2023_01_30
+{
+	void fWrongLoopTest1(void)
+	{
+		int nData{};
+
+		scanf_s("%d", &nData);
+
+		while (nData < 10)
+		{
+			printf("Âü");
+			nData = 10;
+		}
+	}
+
+	void fBasicLoopTest1(void)
+	{
+		int nData{};
+
+		scanf_s("%d", &nData);
+		while (nData < 10)
+		{
+			printf("%d\n", nData);
+			nData++;
+		}
+
+		for (int i = 0; i < 10; i++)
+		{
+			printf("%d\n", i);
+		}
+	}
+
+	void fBasicLoop2(void)
+	{
+		int nBigNumber{};
+		int nSmallNumber{};
+
+		scanf_s("%d", &nSmallNumber);
+		scanf_s("%d", &nBigNumber);
+
+		if (nBigNumber < nSmallNumber)
+		{
+			int nNumberForSwap = nBigNumber;
+			nBigNumber = nSmallNumber;
+			nSmallNumber = nNumberForSwap;
+		}
+
+		int nIterator = nSmallNumber;
+		while (nIterator <= nBigNumber)
+		{
+			printf("while : %d\n", nIterator);
+
+			nIterator++;
+		}
+
+		for (int nIterator = nSmallNumber; nIterator <= nBigNumber; nIterator++)
+			printf("for : %d\n", nIterator);
+	}
+}
