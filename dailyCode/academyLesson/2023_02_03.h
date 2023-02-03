e36a76d892a5b9d39ebcb8ea190c23daa604c5a5
henongj <idjimj1@naver.com>
2023-02-03 15:33:55 +0900

    academy lecture 2023_02_03 OX찍기

--- dailyCode/academyLesson/2023_02_03.h
+++ dailyCode/academyLesson/2023_02_03.h
@@ -2,6 +2,37 @@
 
 namespace academyLecture_2023_02_03
 {
+	void fPrintPattern(void)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				if (i == j ||
+					i + j == 8 ||
+					j - i == 4 ||
+					i - j == 4 ||
+					i + j == 4 ||
+					i + j == 12)
+					printf("X");
+				else
+					printf("O");
+			}
+			printf("\n");
+		}
+		printf("\n");
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				if ((i % 4 == j % 4) || ((i % 4) + (j % 4) == 4))
+					printf("X");
+				else
+					printf("O");
+			}
+			printf("\n");
+		}
+	}
 	void fPrintOXWithTwoFor(void)
 	{
 		for (int i = 0; i < 5; i++)
