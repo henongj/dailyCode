a02a9cf78253055c4a328c9766f2301629b2c9f5
henongj <idjimj1@naver.com>
2023-02-08 15:51:53 +0900

    2023_02_08 lecture 반복문, 주의할 점. 강의 txt 참

--- dailyCode/academyLesson/2023_02_08.h
+++ dailyCode/academyLesson/2023_02_08.h
@@ -18,4 +18,46 @@ namespace academyLecture_2023_02_07
 			printf("\n");
 		}
 	}
+	void fArrayFill(void)
+	{
+		int arData[3][4]{};
+		int nNumber{};
+
+		nNumber = 1;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				arData[i][j] = nNumber;
+				nNumber++;
+			}
+		}
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				printf("%3d ", arData[i][j]);
+			}
+			printf("\n");
+		}
+
+		nNumber = 1;
+		for (int i = 0; i < 12; i++)
+		{
+			int nIndex1 = i / 4;
+			int nIndex2 = i % 4;
+			arData[nIndex1][nIndex2] = nNumber;
+			nNumber++;
+		}
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				printf("%3d ", arData[i][j]);
+			}
+			printf("\n");
+		}
+	}
 }
