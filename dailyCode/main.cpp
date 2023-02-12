562bd1dc17318963fb1735ada356a13cdf2c9973
henongj <idjimj1@naver.com>
2023-02-12 21:28:02 +0900

    acmicpc 단계별로 풀어보기 배열 1번

--- dailyCode/main.cpp
+++ dailyCode/main.cpp
@@ -8,16 +8,34 @@
 #include<vector>
 #include<sstream>
 #include<stack>
-#include<cmath>	
+#include<cmath>
 #include<set>
-#include<unordered_set>
 using namespace std;
 
+//https://www.acmicpc.net/problem/10807
+//개수 세기
+
+void fDataInput(int& nN, int& nInput1, int& nInput2, int& nTargetNumberCount, vector<int>& vNumberCount)
+{
+	cin >> nN;
+	vNumberCount.resize(200);
+	for (int i = 0; i < nN; i++)
+	{
+		cin >> nInput1;
+		vNumberCount[nInput1 + 100] = vNumberCount[nInput1 + 100] + 1;
+	}
+	cin >> nInput2;
+}
 int main(void)
 {
+	int nN{};
+	int nInput1{};
+	int nInput2{};
+	int nTargetNumberCount{};
+	vector<int> vNumberCount{};
 
+	fDataInput(nN, nInput1, nInput2, nTargetNumberCount, vNumberCount);
+	nTargetNumberCount = vNumberCount[nInput2 + 100];
+
+	printf("%d", nTargetNumberCount);
 }
-
-
-
-
