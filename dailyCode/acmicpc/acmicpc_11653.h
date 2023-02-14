1ae6b70363b2467493c45e43bb0466979d2c400b
henongj <idjimj1@naver.com>
2023-02-14 18:03:43 +0900

    acmicpc 단계별로 풀어보기 기본수학 2 3번문제

---
+++ dailyCode/acmicpc/acmicpc_11653.h
@@ -0,0 +1,44 @@
+#include<algorithm>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<string>
+#include<unordered_map>
+#include<utility>
+#include<vector>
+#include<sstream>
+#include<stack>
+#include<cmath>
+#include<set>
+using namespace std;
+
+//https://www.acmicpc.net/problem/11653
+//소인수분해
+
+void fDataInput(int& nData, int& nDivisor)
+{
+	cin >> nData;
+	nDivisor = 2;
+}
+
+void fGetAllDivisor(int nData, int nDivisor)
+{
+	while (nData != 1)
+	{
+		if (nData % nDivisor == 0)
+		{
+			cout << nDivisor << endl;
+			nData /= nDivisor;
+		}
+		else
+			nDivisor++;
+	}
+}
+
+void solution(void)
+{
+	int nData{};
+	int nDivisor{};
+	fDataInput(nData, nDivisor);
+	fGetAllDivisor(nData, nDivisor);
+}
