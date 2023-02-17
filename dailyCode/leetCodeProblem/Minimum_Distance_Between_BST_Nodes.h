58ca113888c98876a213e26af9707e3a50a7c64d
henongj <idjimj1@naver.com>
2023-02-17 19:58:55 +0900

    daily leet code 오답

---
+++ dailyCode/leetCodeProblem/Minimum_Distance_Between_BST_Nodes.h
@@ -0,0 +1,43 @@
+
+//https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+//783. Minimum Distance Between BST Nodes
+
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+
+/* 肋给等 立辟过
+class Solution {
+public:
+	int minDiffInBST(TreeNode* root) {
+		int nMinimumDifference{};
+		int nValue1{};
+		int nValue2{};
+		priority_queue<int, vector<int>, greater<int>> pq;
+		funcInorderBinarySearchTraversal(root, pq);
+
+		nValue1 = pq.top();
+		pq.pop();
+		nValue2 = pq.top();
+		pq.pop();
+		cout << nValue1 << " " << nValue2 << endl;
+		nMinimumDifference = nValue2 - nValue1;
+
+		return nMinimumDifference;
+	}
+	void funcInorderBinarySearchTraversal(TreeNode* root, priority_queue<int, vector<int>, greater<int>>& pq) {
+		if (root->left!= nullptr)
+			funcInorderBinarySearchTraversal(root->left, pq);
+		pq.push(root->val);
+		if (root->right != nullptr)
+			funcInorderBinarySearchTraversal(root->right, pq);
+	}
+};
+*/
