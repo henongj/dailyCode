b66dabfbeb7c88a5c76d7157c50ca73261e311da
henongj <idjimj1@naver.com>
2023-02-20 15:04:14 +0900

    academy lecture 02 20

--- dailyCode/academyLesson/2023_02_20.h
+++ dailyCode/academyLesson/2023_02_20.h
@@ -5,6 +5,14 @@
 namespace academyLecture_2023_02_20
 {
 	void printData(const int* arData, int nLength);
+	// 자 int** p 에서 *p가 배열일까 **p가 배열일까.
+	void exampleFunc1(int** p, int nSize);
+	// 자료형이 꼬일 것 같으면 이렇게 해준다. 그리고 이러면 번거로우니 이렇게 안 만든다
+	void exampleFunc2(int(*p)[], int nSize);
+
+	void getSmallestNumber(int nNumber1, int nNumber2, int nNumber3, int* nSmallest);
+	void getSumOfArray(const int* arrNumbers, const int nSize, int* nSum);
+	void swapTwoNumber(int* nNumber1, int* nNumber2);
 
 	int funcMain(void)
 	{
@@ -13,7 +21,17 @@ namespace academyLecture_2023_02_20
 		printData(arMain1, 3);
 		printData(arMain2, 5);
 
+		int nSmallest{};
+		int nN{};
+		nN = 23;
+		getSmallestNumber(13, 5, 7, &nSmallest);
+		printf("Smallest number is %d\n", nSmallest);
 
+		getSumOfArray(arMain2, 5, &nSmallest);
+		printf("Sum of array is %d\n", nSmallest);
+
+		swapTwoNumber(&nSmallest, &nN);
+		printf("Swapped number is %d, %d\n", nSmallest, nN);
 	}
 
 	void printData(const int* arData, int nLength)
@@ -22,4 +40,28 @@ namespace academyLecture_2023_02_20
 			printf("%d ", arData[i]);
 		puts("");
 	}
+
+	void getSmallestNumber(int nNumber1, int nNumber2, int nNumber3, int* nSmallest)
+	{
+		*nSmallest = nNumber1;
+		if (*nSmallest > nNumber2)
+			*nSmallest = nNumber2;
+		if (*nSmallest > nNumber3)
+			*nSmallest = nNumber3;
+	}
+
+	void getSumOfArray(const int* arrNumbers, int nSize, int* nSum)
+	{
+		*nSum = 0;
+		for (int i = 0; i < nSize; i++)
+			*nSum += arrNumbers[i];
+	}
+
+	void swapTwoNumber(int* nNumber1, int* nNumber2)
+	{
+		int nTemp = *nNumber1;
+		*nNumber1 = *nNumber2;
+		*nNumber2 = nTemp;
+	}
+
 }
