f936bba0e8cf1af1d7d87e4309554efa3048a83b
henongj <idjimj1@naver.com>
2023-03-16 15:46:00 +0900

    3월 16일 강의, 레퍼런스 설명

--- academyClassLesson/academyClassLesson/academyClassLesson/230316_reference.h
+++ academyClassLesson/academyClassLesson/academyClassLesson/230316_reference.h
@@ -1,35 +1,35 @@
 #pragma once
 #include<iostream>
 
-bool getSum1(int nArr[], int nArrLength, int& nSum);
-bool getSum2(int nArr[], int nArrLength, int* nSum);
-int getSum3(int nArr[], int nArrLength);
-int getSum4(int nArr[], int nArrLength, int* nSum);
-int getSum5(int nArr[], int nArrLength, int* nSum);
+bool getSum1(const int nArr[], int nArrLength, int& nSum);
+bool getSum2(const int nArr[], int nArrLength, int* pSum);
+int getSum3(const int nArr[], int nArrLength);
+int getSum4(const int nArr[], int nArrLength, int* pSum);
+int getSum5(const int nArr[], int nArrLength, int* pSum);
 
 bool swapNumber(int& nNumber1, int& nNumber2);
 bool swapNumber(int* nNumber1, int* nNumber2);
 
 bool interpolate1(int nNumber1, int nNumber2, float fRatio, int& nResult);
-bool interpolate2(int nNumber1, int nNumber2, float fRatio, int* nResult);
+bool interpolate2(int nNumber1, int nNumber2, float fRatio, int* pResult);
 int interpolate3(int nNumber1, int nNumber2, float fRatio);
-int interpolate4(int nNumber1, int nNumber2, float fRatio, int* nResult);
+int interpolate4(int nNumber1, int nNumber2, float fRatio, int* pResult);
 int interpolate5(int nNumber1, int nNumber2, float fRatio, int& nResult);
 
 void stringUpper1(char* strData);
-char* stringUppder3(char* strLower);
+bool stringUppder3(const char* strLower, char* strResult, int);
 
 bool getRoot1(float fValue, float& fRoot);
-bool getRoot2(const float* fValue, float& fRoot);
+bool getRoot2(const float* pValue, float& fRoot);
 bool getRoot3(const float& fValue, float& fRoot);
-bool getRoot4(float fValue, float* fRoot);
-bool getRoot5(const float* fValue, float* fRoot);
-bool getRoot6(const float& fValue, float* fRoot);
+bool getRoot4(float fValue, float* pRoot);
+bool getRoot5(const float* pValue, float* pRoot);
+bool getRoot6(const float& fValue, float* pRoot);
 float getRoot7(float fValue);
-float getRoot8(float* fValue);
+float getRoot8(float* pValue);
 float getRoot9(float& fValue);
-float getRoot10(float fValue, float& fRoot);
-float getRoot11(float fValue, float* fRoot);
+void getRoot10(float fValue, float& fRoot);
+void getRoot11(float fValue, float* pRoot);
 
 //이런거 하지마라
 template<int nLength>
