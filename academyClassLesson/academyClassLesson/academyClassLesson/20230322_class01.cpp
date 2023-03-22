42f4db01a8176e51141da09a99b2136dc2e833f7
henongj <idjimj1@naver.com>
2023-03-22 14:59:39 +0900

    얕은 복사 깊은 복사의 문제점이야기

---
+++ academyClassLesson/academyClassLesson/academyClassLesson/20230322_class01.cpp
@@ -0,0 +1,23 @@
+#include "20230322_class01.h"
+
+C_20230322_class01::C_20230322_class01() :
+	m_pData{}
+{
+	m_pData = new int{};
+}
+
+C_20230322_class01::~C_20230322_class01()
+{
+	delete m_pData;
+	m_pData = nullptr;
+}
+
+void C_20230322_class01::setData(int nData)
+{
+	*m_pData = nData;
+}
+
+int C_20230322_class01::getData()
+{
+	return *m_pData;
+}
---
+++ academyClassLesson/academyClassLesson/academyClassLesson/20230322_class01.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class C_20230322_class01
+{
+private:
+	int* m_pData;
+
+public:
+	C_20230322_class01();
+	~C_20230322_class01();
+	
+	void setData(int nData);
+	int getData();
+};
