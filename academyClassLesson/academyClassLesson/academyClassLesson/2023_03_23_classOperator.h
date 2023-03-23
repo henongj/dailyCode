6a3564a83447a2594500864e1cec65116f2120e9
henongj <idjimj1@naver.com>
2023-03-23 15:48:34 +0900

    빈 클래스의 기본형태

--- academyClassLesson/academyClassLesson/academyClassLesson/2023_03_23_classOperator.h
+++ academyClassLesson/academyClassLesson/academyClassLesson/2023_03_23_classOperator.h
@@ -15,3 +15,15 @@ public:
 	C20230323ClassOperator& operator = (int nData);
 	C20230323ClassOperator& operator = (C20230323ClassOperator& cData);
 };
+
+
+// 이게 빈 생성자를 만드는 법이다.
+class C20230323EmptyClass
+{
+private:
+public:
+	C20230323EmptyClass() = default;
+	C20230323EmptyClass(const C20230323EmptyClass&) = delete;
+	C20230323EmptyClass& operator=(const C20230323EmptyClass&) = delete;
+	
+};
