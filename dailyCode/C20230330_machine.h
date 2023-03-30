39da5694330b962f60586bb9094c3e31de18b227
henongj <idjimj1@naver.com>
2023-03-30 22:40:22 +0900

    클래스 작성의 전형적 형태, 기본 문법

--- dailyCode/C20230330_machine.h
+++ dailyCode/C20230330_machine.h
@@ -10,6 +10,16 @@ class C20230330_machine_interface abstract
 	virtual void setFuel(int nFuel) = 0;
 };
 
+// __interface로 인터페이스 만들기
+__interface C20230330_machine_interface2
+{
+	void start();
+	void start();
+
+	int* getFuel(int nFuel);
+	void setFuel(int nFuel);
+};
+
 // 인터페이스를 상속받아서 추상 클래스 만들기
 class C20230330_machine abstract : public C20230330_machine_interface
 {
@@ -20,4 +30,16 @@ public:
 	C20230330_machine() = default;
 	C20230330_machine(const C20230330_machine&) = delete;
 	C20230330_machine& operator=(const C20230330_machine&) = delete;
+};
+
+// __interface로 인터페이스를 상속받아서 추상 클래스 만들기
+class C20230330_machine2 abstract : public C20230330_machine_interface2
+{
+private:
+	int m_nFuel;
+public:
+	virtual ~C20230330_machine2() = default;
+	C20230330_machine2() = default;
+	C20230330_machine2(const C20230330_machine2&) = delete;
+	C20230330_machine2& operator=(const C20230330_machine2&) = delete;
 };
