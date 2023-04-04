ce55daf189ea4941fff47a70e82725be867f0388
henongj <idjimj1@naver.com>
2023-04-04 20:38:33 +0900

    전략패턴 연습. 망한 것 같다

---
+++ dailyCode/C20230404_strategyClass.cpp
@@ -0,0 +1,19 @@
+#include "C20230404_strategyClass.h"
+
+void C20230404_UNIT::init()
+{
+	
+}
+
+void C20230404_UNIT::release()
+{
+}
+
+void C20230404_UNIT::setMove(C20230404_MOVE* move)
+{
+}
+
+C20230404_MOVE* C20230404_UNIT::getMove()
+{
+	return nullptr;
+}
