fcdde8bec0cef413c3b33abb0ff2926ba766db96
henongj <idjimj1@naver.com>
2023-04-06 14:41:14 +0900

    bubble sort

--- academyClassLesson/academyClassLesson/AcademyAlgorithmLesson/bubbleSort.cpp
+++ academyClassLesson/academyClassLesson/AcademyAlgorithmLesson/bubbleSort.cpp
@@ -1,6 +1,6 @@
 #include "bubbleSort.h"
 #include<stdio.h>
-void n20230406_bubbleSort::swapInt(int& nData1, int& nData2)
+void n20230406_bubbleSort_01::swapInt(int& nData1, int& nData2)
 {
 	int nTemp{};
 	nTemp = nData1;
@@ -8,7 +8,7 @@ void n20230406_bubbleSort::swapInt(int& nData1, int& nData2)
 	nData2 = nTemp;
 }
 
-void n20230406_bubbleSort::bubbleSort(int* pArrayList, int nArrayLength)
+void n20230406_bubbleSort_01::bubbleSort(int* pArrayList, int nArrayLength)
 {
 	while (nArrayLength > 1)
 	{
@@ -17,7 +17,7 @@ void n20230406_bubbleSort::bubbleSort(int* pArrayList, int nArrayLength)
 	}
 }
 
-void n20230406_bubbleSort::bubbleSortLogic(int* pArrayList, int nArrayLength)
+void n20230406_bubbleSort_01::bubbleSortLogic(int* pArrayList, int nArrayLength)
 {
 	for (int i = 0; i < nArrayLength - 1; i++)
 	{
@@ -26,14 +26,14 @@ void n20230406_bubbleSort::bubbleSortLogic(int* pArrayList, int nArrayLength)
 	}
 }
 
-void n20230406_bubbleSort::printArray(int* pArrayList, int nArrayLength)
+void n20230406_bubbleSort_01::printArray(int* pArrayList, int nArrayLength)
 {
 	for (int i = 0; i < nArrayLength; i++)
 		printf("%d ", pArrayList[i]);
 	printf("\n");
 }
 
-void n20230406_bubbleSort::runSpace(void)
+void n20230406_bubbleSort_01::runSpace(void)
 {
 	const int nArrayLength = 10;
 	int nArrayList[nArrayLength] = { 5, 66, 12, 4, 9, 11, 16, 652, 97, 10 };
@@ -41,4 +41,52 @@ void n20230406_bubbleSort::runSpace(void)
 	printArray(nArrayList, nArrayLength);
 	bubbleSort(nArrayList, nArrayLength);
 	printArray(nArrayList, nArrayLength);
+}
+
+void n20230406_bubbleSort_02::swapInt(int& nData1, int& nData2)
+{
+	int nTemp{};
+	nTemp = nData1;
+	nData1 = nData2;
+	nData2 = nTemp;
+}
+
+void n20230406_bubbleSort_02::bubbleSort(int* pArrayList, int nArrayLength, bool isAscending)
+{
+	while (nArrayLength > 1)
+	{
+		bubbleSortLogic(pArrayList, nArrayLength, isAscending);
+		nArrayLength--;
+	}
+}
+
+void n20230406_bubbleSort_02::bubbleSortLogic(int* pArrayList, int nArrayLength, bool isAscending)
+{
+	for (int i = 0; i < nArrayLength - 1; i++)
+	{
+		if ((isAscending && pArrayList[i] > pArrayList[i + 1]) || (!isAscending && pArrayList[i] < pArrayList[i + 1]))
+			swapInt(pArrayList[i], pArrayList[i + 1]);
+	}
+}
+
+
+void n20230406_bubbleSort_02::printArray(const int* pArrayList, int nArrayLength)
+{
+	for (int i = 0; i < nArrayLength; i++)
+		printf("%d ", pArrayList[i]);
+	printf("\n");
+}
+
+void n20230406_bubbleSort_02::runSpace(void)
+{
+
+	const int nArrayLength = 10;
+	int nArrayList[nArrayLength] = { 5, 66, 12, 4, 9, 13, 16, 652, 97, 10 };
+	
+	bool isAcending{};
+	isAcending = false;
+
+	n20230406_bubbleSort_02::printArray(nArrayList, nArrayLength);
+	n20230406_bubbleSort_02::bubbleSort(nArrayList, nArrayLength, isAcending);
+	n20230406_bubbleSort_02::printArray(nArrayList, nArrayLength);
 }
--- academyClassLesson/academyClassLesson/AcademyAlgorithmLesson/bubbleSort.h
+++ academyClassLesson/academyClassLesson/AcademyAlgorithmLesson/bubbleSort.h
@@ -1,10 +1,18 @@
 #pragma once
 
-namespace n20230406_bubbleSort
+namespace n20230406_bubbleSort_01
 {
 	void swapInt(int& nData1, int& nData2);
 	void bubbleSort(int* pArrayList, int nArrayLength);
 	void bubbleSortLogic(int* pArrayList, int nArrayLength);
 	void printArray(int* pArrayList, int nArrayLength);
+	void runSpace(void);
+}
+namespace n20230406_bubbleSort_02
+{
+	void swapInt(int& nData1, int& nData2);
+	void bubbleSort(int* pArrayList, int nArrayLength, bool isAscending);
+	void bubbleSortLogic(int* pArrayList, int nArrayLength, bool isAscending);
+	void printArray(const int* pArrayList, int nArrayLength);
 	void runSpace(void);
 }
