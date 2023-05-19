1c597f3a6240a8b367ad28f287aba422cfe14f81
henongj <idjimj1@naver.com>
2023-05-19 15:50:08 +0900

    template start

---
+++ academyClassLesson/academyClassLesson/academyClassLesson/20230519_templateAdd.cpp
@@ -0,0 +1,8 @@
+#include"template20230519_add.h"
+
+
+//template<typename T>
+//T add(T t1, T t2)
+//{
+//	return t1 + t2;
+//}
---
+++ academyClassLesson/academyClassLesson/academyClassLesson/template20230519_add.h
@@ -0,0 +1,11 @@
+#pragma once
+
+template<typename T>
+T add(T t1, T t2);
+
+
+template<typename T>
+T add(T t1, T t2)
+{
+	return t1 + t2;
+}
