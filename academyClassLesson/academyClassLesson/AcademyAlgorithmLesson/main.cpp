bb6a11e53fb13bdd4df8538f4f91f867818368cc
henongj <idjimj1@naver.com>
2023-05-20 22:02:42 +0900

    merge

--- academyClassLesson/academyClassLesson/AcademyAlgorithmLesson/main.cpp
+++ academyClassLesson/academyClassLesson/AcademyAlgorithmLesson/main.cpp
@@ -1,22 +1,8 @@
 #include<iostream>
-#include"20230509_BST_DoublePointer.h"
+
 int main(void)
 {
-	C20230509_BinarySearchTree cBST{};
-	//4 1 9 8 3 5 2 6 7
-	cBST.add(4);
-	cBST.add(1);
-	cBST.add(9);
-	cBST.add(8);
-	cBST.add(3);
-	cBST.add(5);
-	cBST.add(2);
-	cBST.add(6);
-	cBST.add(7);
 	
-	cBST.print();
-	cBST.erase(2);
-	cBST.print();
 
 	return 0;
 }
--- academyClassLesson/academyClassLesson/academyClassLesson/main.cpp
+++ academyClassLesson/academyClassLesson/academyClassLesson/main.cpp
@@ -1,15 +1,6 @@
 #include<iostream>
-#include<Windows.h>
-#include"20230517_baseBall.h"
 
 int main(void)
 {
-	srand(timeGetTime());
-
-	C20230517_baseBall cBaseBall;
-
-	cBaseBall.init(4);
-	
 	return 0;
 }
-
