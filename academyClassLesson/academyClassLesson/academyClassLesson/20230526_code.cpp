47223722145d2e813c0b7b4efd70a2779e1eb5a7
henongj <idjimj1@naver.com>
2023-05-29 14:54:37 +0900

    26일 코드

---
+++ academyClassLesson/academyClassLesson/academyClassLesson/20230526_code.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<map>
+#include"20230526_studentManager.h"
+
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+int mainSpace01_20230526(void)
+{
+	
+
+	return 0;
+}
