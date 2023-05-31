1b6219848a12466057c5c13ad50635fd8d50a365
henongj <idjimj1@naver.com>
2023-05-31 14:29:08 +0900

    옵저버 패턴

--- academyClassLesson/academyClassLesson/academyClassLesson/20230530_Subject.h
+++ academyClassLesson/academyClassLesson/academyClassLesson/20230530_Subject.h
@@ -13,6 +13,7 @@ class C20230530_SUBJECT abstract : public I20230530_SUBJECT
 {
 public:
 	virtual ~C20230530_SUBJECT() = default;
+	
 };
 
 class C20230530_PLAYER : public C20230530_SUBJECT
@@ -21,9 +22,9 @@ private:
 	std::multiset<C20230530_OBSERVER*> m_setObserver;
 	int m_nCount{};
 
-	// C20230530_SUBJECT을(를) 통해 상속됨
+public:
 	virtual void add(C20230530_OBSERVER* pObserver, int nCount) override;
 	virtual void remove(C20230530_OBSERVER* pObserver, int nCount = 1) override;
 	virtual void notify(int nData) override;
-
+	
 };
