f0fd10726e47c9dcda58e3adf0767d2d61a1d385
henongj <idjimj1@naver.com>
2023-06-25 19:44:30 +0900

    댓글시스템 테스트. 원하는 모양대로 나오지 않는다. fmkorea처럼 나오게 하려면 순회방법을 나눠야할듯

--- dailyCode/main.cpp
+++ dailyCode/main.cpp
@@ -1,6 +1,6 @@
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
+//#define _CRTDBG_MAP_ALLOC
+//#include <stdlib.h>
+//#include <crtdbg.h>
 #include<algorithm>
 #include<iostream>
 #include<map>
@@ -14,23 +14,38 @@
 #include<cmath>
 #include<set>
 
-#include"20230530_character.h"
+#include"commentTree.h"
 
 int main(void)
 {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	C20230530_Character character{};
-	character.init();
-
-	C20230530_Move* pMove = character.createMove(C20230530_Character::E20230530_MoveType::LAND);
-
-	pMove->printMoveSound();
-	pMove->printMoveSpeed();
-
-	delete pMove;
+	c_CommentTree commentTree{};
+	commentTree.init();
+	std::string strInput{};
 	
-	character.release();
-	
+	strInput = "this is the 0 comment";
+	commentTree.addComment(strInput, c_CommentTree::e_CommentType::Comment);
+	strInput = "this is the 1 comment";
+	commentTree.addComment(strInput, c_CommentTree::e_CommentType::Comment);
+	strInput = "this is the 2 comment";
+	commentTree.addComment(strInput, c_CommentTree::e_CommentType::Comment);
+	strInput = "this is the 3 comment";
+	commentTree.addComment(strInput, c_CommentTree::e_CommentType::Comment);
+
+	strInput = "this is the 4 comment, 1 Reply";
+	commentTree.addReply(1, strInput, c_CommentTree::e_CommentType::Reply);
+	strInput = "this is the 5 comment, 2 Reply";
+	commentTree.addReply(1, strInput, c_CommentTree::e_CommentType::Reply);
+	strInput = "this is the 6 comment, 3 Reply";
+	commentTree.addReply(4, strInput, c_CommentTree::e_CommentType::Reply);
+	strInput = "this is the 7 comment, 4 Reply";
+	commentTree.addReply(5, strInput, c_CommentTree::e_CommentType::Reply);
+	strInput = "this is the 8 comment, 5 Reply";
+	commentTree.addReply(4, strInput, c_CommentTree::e_CommentType::Reply);
+
+	commentTree.printComment();
+
+	commentTree.release();
 	return 0;
 }
