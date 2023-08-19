23e1f0d0be3b169274aef5dc2ea66ff56800615f
henongj <idjimj1@naver.com>
2023-08-19 22:20:17 +0900

    file : 오답. 같은 거리 내 우선순위 에러. 좌하단이 같은 높이 오른쪽을 우선함

--- dailyCode/acmicpc_16236.h
+++ dailyCode/acmicpc_16236.h
@@ -16,233 +16,153 @@
 
 //https://www.acmicpc.net/problem/16236
 //아기 상어
+
+//https://www.acmicpc.net/problem/16236
+//아기 상어
 namespace acmicpc_16236
 {
 	using namespace std;
 
-	class c_Map
+	struct sInfo
 	{
-	private:
-		vector<vector<int>> m_vvMap{};
-		int m_nSize{};
-
-		vector<vector<bool>> m_vvVisit{};
-		priority_queue<pair<int, int>> m_pqFish{};
-		pair<int, int> m_pSharkPosition{};
-
-	public:
-		c_Map() = default;
-		~c_Map() = default;
-
-		c_Map& operator=(const c_Map& sSrc) = delete;
-		c_Map(const c_Map& sSrc) = delete;
-
-		void init(int nSize);
-		void inputData(void);
-
-		// visit
-		bool isVisited(pair<int, int> pPosition);
-		void visit(pair<int, int> pPosition)
-		{
-			m_vvVisit[pPosition.first][pPosition.second] = true;
-		}
-
-		// get
-		pair<int, int>& getSharkPosition(void)
-		{
-			return m_pSharkPosition;
-		}
-		pair<int, int> getFishPosition(void)
-		{
-			pair<int, int> pPosition = m_pqFish.top();
-			m_pqFish.pop();
-			return pPosition;
-		}
-		vector<vector<int>>& getMap(void)
-		{
-			return m_vvMap;
-		}
-		int getSize(void)
-		{
-			return m_nSize;
-		}
-
-		// set
-		void setSharkPosition(pair<int, int>& pPosition)
-		{
-			m_vvMap[m_pSharkPosition.first][m_pSharkPosition.second] = 0;
-
-			m_pSharkPosition = pPosition;
-
-			m_vvMap[m_pSharkPosition.first][m_pSharkPosition.second] = 9;
-		}
-
-		// else
-		void popFish(void)
-		{
-			m_pqFish.pop();
-		}
-		void resetVisit(void);
-		bool isLeftFish(void)
-		{
-			return !m_pqFish.empty();
-		}
-	};
-	class c_BabyShark
-	{
-	private:
-		pair<int, int> m_pPosition{};
-		int m_nSize{};
-		int m_nEatCount{};
-		int m_nTime{};
-
-	public:
-		c_BabyShark() = default;
-		~c_BabyShark() = default;
-
-		c_BabyShark& operator=(const c_BabyShark& sSrc) = delete;
-		c_BabyShark(const c_BabyShark& sSrc) = delete;
-
-		void init(pair<int, int> pPosition);
-		void eatFish(void);
-		bool move(c_Map& cMap);
-
-		// get
-		pair<int, int>& getPosition(void)
-		{
-			return m_pPosition;
-		}
-		int getTime(void)
-		{
-			return m_nTime;
-		}
-
+		int nY;
+		int nX;
+		int nTime;
 	};
 	void solution(void);
+	void inputData(int& nSize, vector<vector<int>>& vvMap, pair<int, int>& pSharkPos);
+	bool isInside(int nX, int nY, int nSize);
+	void resetVisited(vector<vector<bool>>& vvVisited, int nSize);
+	void printMap(vector<vector<int>>& vvMap, int nSize);
 
 	void solution(void)
 	{
-		c_BabyShark cBabyShark{};
-		c_Map cMap{};
+		int nSize{};
+		vector<vector<int>> vvMap{};
+		pair<int, int> pSharkPos{};
 
-		int nSize = 0;
+		int nSharkSize{};
+		int nEatCount{};
+		int nTime{};
 
-		cin >> nSize;
-		cMap.init(nSize);
-		cMap.inputData();
+		// 상 좌 우 하
+		int arrDir[4][2] = { {-1,0}, {0,-1}, {0,1}, {1,0} };
+		queue<sInfo> qInfo{};
+		vector<vector<bool>> vvVisited{};
 
-		cBabyShark.init(cMap.getSharkPosition());
+		inputData(nSize, vvMap, pSharkPos);
+		vvVisited.resize(nSize);
+		for (int i = 0; i < nSize; ++i)
+			vvVisited[i].resize(nSize);
 
-		while (cMap.isLeftFish())
+		nSharkSize = 2;
+		nEatCount = 0;
+		nTime = 0;
+
+		vvMap[pSharkPos.first][pSharkPos.second] = 0;
+		qInfo.push({ pSharkPos.first, pSharkPos.second, nTime });
+
+		//bfs
+		while (!qInfo.empty())
 		{
-			if (cBabyShark.move(cMap) == false)
-				break;
-		}
+			sInfo sCurrentInfo = qInfo.front();
+			qInfo.pop();
 
-		cout << cBabyShark.getTime() << endl;
+			int nCurrentY = sCurrentInfo.nY;
+			int nCurrentX = sCurrentInfo.nX;
+			int nCurrentTime = sCurrentInfo.nTime;
 
-	}
-	void c_BabyShark::init(pair<int, int> pPosition)
-	{
-		m_pPosition = pPosition;
-		m_nSize = 2;
-		m_nEatCount = 0;
-		m_nTime = 0;
-	}
-	void c_BabyShark::eatFish(void)
-	{
-		m_nEatCount++;
-		if (m_nEatCount == m_nSize)
-		{
-			m_nSize++;
-			m_nEatCount = 0;
-		}
-	}
-	bool c_BabyShark::move(c_Map& cMap)
-	{
-		pair<int, int> pPosition = m_pPosition;
-		pair<int, int> pNextPosition = cMap.getFishPosition();
-		vector<vector<int>>& vvMap = cMap.getMap();
-		int nFishSize = vvMap[pNextPosition.first][pNextPosition.second];
-
-		if (nFishSize >= m_nSize)
-			return false;
-
-		int nTime = 0;
-		cMap.resetVisit(); // visit 초기화
-		cMap.visit(pPosition); // 현재 위치 visit
-		int arrDir[4][2] = { {-1,0},{0,1},{0,-1},{1,0} };
-
-		// queue를 이용한 BFS로 수정해야함
-		while (pPosition.first != pNextPosition.first || pPosition.second != pNextPosition.second)
-		{
-			// vvMap[row][col]에 적힌 숫자가 m_nSize이하여야 이동 가능
-			// 이동 가능한 위치가 여럿이면 위, 오른쪽, 왼쪽, 아래 순서로 이동
-
-			// 4방향 탐색
-			for (int i = 0; i < 4; i++)
+			if (vvMap[nCurrentY][nCurrentX] != 0 && vvMap[nCurrentY][nCurrentX] < nSharkSize)
 			{
-				int nRow = pPosition.first + arrDir[i][0];
-				int nCol = pPosition.second + arrDir[i][1];
+				vvMap[nCurrentY][nCurrentX] = 0;
+				nEatCount++;
+				nTime = nCurrentTime;
 
-				// 범위 내이고 방문하지 않음
-				if (0 <= nRow && nRow < cMap.getSize() && 0 <= nCol && nCol < cMap.getSize() && cMap.isVisited({ nRow,nCol }) == false)
+				cout << "\neat : " << nCurrentY << " " << nCurrentX << " " << nCurrentTime << " " << nSharkSize << endl;
+				printMap(vvMap, nSize);
+
+				if (nEatCount == nSharkSize)
 				{
-					// 이동 가능한 위치이면 이동
-					if (vvMap[nRow][nCol] <= m_nSize)
-					{
-						pPosition = { nRow,nCol };
-						cMap.visit(pPosition);
-						nTime++;
+					nSharkSize++;
+					nEatCount = 0;
+					cout << "size up : " << nSharkSize << endl;
+				}
 
-						// 이동했으면 다른 방향은 탐색할 필요 없음
-						i = 4;
-					}
+				resetVisited(vvVisited, nSize);
+				while (!qInfo.empty())
+					qInfo.pop();
+			}
+
+			for (int i = 0; i < 4; ++i)
+			{
+				int nNextY = nCurrentY + arrDir[i][0];
+				int nNextX = nCurrentX + arrDir[i][1];
+
+				if (isInside(nNextX, nNextY, nSize) && !vvVisited[nNextY][nNextX] && vvMap[nNextY][nNextX] <= nSharkSize)
+				{
+					vvVisited[nNextY][nNextX] = true;
+					qInfo.push({ nNextY, nNextX, nCurrentTime + 1 });
 				}
 			}
 		}
 
-		m_pPosition = pPosition; // 상어 위치 갱신
-		cMap.setSharkPosition(m_pPosition); // 상어 위치 갱신
+		cout << nTime << endl;
 
-		m_nTime += nTime; // 시간 갱신
-		eatFish(); // 물고기 먹음
-		cMap.popFish(); // 물고기 제거
-		return true;
+
+
+
 	}
-	void c_Map::init(int nSize)
+
+	void inputData(int& nSize, vector<vector<int>>& vvMap, pair<int, int>& pSharkPos)
 	{
-		m_nSize = nSize;
-		m_vvMap.resize(nSize, vector<int>(nSize, 0));
-		m_vvVisit.resize(nSize, vector<bool>(nSize, false));
-	}
-	void c_Map::inputData(void)
-	{
-		for (int r = 0; r < m_nSize; r++)
+		cin >> nSize;
+		vvMap.resize(nSize);
+		for (int i = 0; i < nSize; ++i)
 		{
-			for (int c = 0; c < m_nSize; c++)
+			vvMap[i].resize(nSize);
+			for (int j = 0; j < nSize; ++j)
 			{
-				cin >> m_vvMap[r][c];
+				cin >> vvMap[i][j];
 
-				if (m_vvMap[r][c] == 9)
-					m_pSharkPosition = { r,c };
-				else if (m_vvMap[r][c] != 0)//not empty and not shark
-					m_pqFish.push({ r,c });
+				if (vvMap[i][j] == 9)
+				{
+					pSharkPos.first = i;
+					pSharkPos.second = j;
+				}
+
 			}
 		}
 	}
-	void c_Map::resetVisit(void)
+
+	bool isInside(int nX, int nY, int nSize)
 	{
-		for (int r = 0; r < m_nSize; r++)
-		{
-			for (int c = 0; c < m_nSize; c++)
-				m_vvVisit[r][c] = false;
-		}
-	}
-	bool c_Map::isVisited(pair<int, int> pPosition)
-	{
-		if (m_vvVisit[pPosition.first][pPosition.second])
+		if (nX >= 0 && nX < nSize && nY >= 0 && nY < nSize)
 			return true;
 		return false;
 	}
+
+	void resetVisited(vector<vector<bool>>& vvVisited, int nSize)
+	{
+		for (int i = 0; i < nSize; ++i)
+		{
+			for (int j = 0; j < nSize; ++j)
+			{
+				vvVisited[i][j] = false;
+			}
+		}
+	}
+
+	void printMap(vector<vector<int>>& vvMap, int nSize)
+	{
+		cout << endl;
+		for (int i = 0; i < nSize; ++i)
+		{
+			for (int j = 0; j < nSize; ++j)
+			{
+				cout << vvMap[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
 }
