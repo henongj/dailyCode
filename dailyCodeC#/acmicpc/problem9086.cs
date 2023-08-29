f21d782f6a878fef44867fc006434f8596b2f140
henongj <idjimj1@naver.com>
2023-08-29 13:24:48 +0900

    file : 9086번 기초 string 문제.

---
+++ dailyCodeC#/acmicpc/problem9086.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace acmicpc_9086
+{
+    class Problem_9086
+    {
+        public void Solution()
+        {
+            List<string> strings = new List<string>();
+
+            strings.Capacity = 1000;
+
+            DataInput(strings);
+
+            int count = strings.Count;
+
+            for (int i = 0; i < strings.Count; i++)
+            {
+                char first = strings[i][0];
+                char last = strings[i][strings[i].Length - 1];
+
+                Console.WriteLine(first.ToString() + last.ToString());
+            }
+
+        }
+        public void DataInput(List<string> inputList)
+        {
+            int inputCount = 0;
+            string? input = "";
+            input = Console.ReadLine();
+
+            inputCount = Convert.ToInt32(input);
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                string str = Console.ReadLine();
+                inputList.Add(str);
+            }
+
+        }
+    }
+}
