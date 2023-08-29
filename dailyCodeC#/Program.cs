16bfacc56ac621c027b9eec1cfbaa5de31800d87
henongj <idjimj1@naver.com>
2023-08-29 13:24:32 +0900

    main : 커밋

---
+++ dailyCodeC#/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+
+namespace mainSpace
+{
+    class mainClass
+    {
+        public static void Main(string[] consoleInput)
+        {
+        }
+
+    }
+}
+
