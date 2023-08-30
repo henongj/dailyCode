61b1dc7c757c726c4559f0448cc8f8073254b05c
henongj <idjimj1@naver.com>
2023-08-31 04:45:25 +0900

    file : C# 간단한 List Dictionary 사용문제. 어렵게 생각하다 시간이 오래걸림

---
+++ dailyCodeC#/acmicpc/problem1236.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acmicpc1236
+{
+    //https://www.acmicpc.net/problem/1236
+    //성 지키기
+
+    public class problem_1236
+    {
+        int nRow;
+        int nCol;
+        List<string> matrix { get; set; } = new List<string>();
+        int nGuardNeed = 0;
+        //hash table
+        Dictionary<int, int> dicNoGuardRow = new Dictionary<int, int>();
+        Dictionary<int, int> dicNoGuardCol = new Dictionary<int, int>();
+
+        public void solution_1236()
+        {
+            inputData();
+
+            nGuardNeed = 0;
+
+            nGuardNeed += dicNoGuardRow.Count;
+            nGuardNeed += dicNoGuardCol.Count;
+
+            nGuardNeed -= Math.Min(dicNoGuardRow.Count, dicNoGuardCol.Count);
+
+            Console.WriteLine(nGuardNeed);
+
+        }
+
+        private void inputData()
+        {
+            string[]? strInput = Console.ReadLine()?.Split(' ');
+
+            if (strInput != null)
+            {
+                nRow = int.Parse(strInput[0]);
+                nCol = int.Parse(strInput[1]);
+            }
+
+            for (int i = 0; i < nRow; i++)
+            {
+                string? strRow = Console.ReadLine();
+                if (strRow != null)
+                {
+                    matrix.Add(strRow);
+                }
+            }
+
+            // 행에 가드가 없는지 확인
+            bool bGuardExist = false;
+            for (int i = 0; i < nRow; i++)
+            {
+                bGuardExist = false;
+                for (int j = 0; j < nCol; j++)
+                {
+                    if (matrix[i][j] == 'X')
+                    {
+                        bGuardExist = true;
+                        break;
+                    }
+                }
+
+                if (bGuardExist == false)
+                {
+                    dicNoGuardRow.Add(i, 0);
+                }
+            }
+
+            // 열에 가드가 없는지 확인
+            for (int i = 0; i < nCol; i++)
+            {
+                bGuardExist = false;
+                for (int j = 0; j < nRow; j++)
+                {
+                    if (matrix[j][i] == 'X')
+                    {
+                        bGuardExist = true;
+                        break;
+                    }
+                }
+
+                if (bGuardExist == false)
+                {
+                    dicNoGuardCol.Add(i, 0);
+                }
+            }
+
+
+        }
+
+    }
+}
