a854cbf1f74f814c1c68a1cdb57df7d1823c6b75
henongj <idjimj1@naver.com>
2023-08-30 16:16:19 +0900

    file : 백준2566번 2차원 배열 사용문제

---
+++ dailyCodeC#/acmicpc/problem2566.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_2566
+{
+    //https://www.acmicpc.net/problem/2566
+    //최댓값
+    class solution_2566
+    {
+        int[,] DataMatrix = new int[9, 9];
+        int max = 0;
+        int MaxPositionX = 0;
+        int MaxPositionY = 0;
+
+        public void Solution()
+        {
+            string? Input = "";
+
+            for (int i = 0; i < 9; i++)
+            {
+                Input = Console.ReadLine();
+                string[] InputArray = new string[9];
+                if (Input != null)
+                {
+                     InputArray = Input.Split(' ');
+                }
+
+                for (int j = 0; j < 9; j++)
+                {
+                    DataMatrix[i, j] = int.Parse(InputArray[j]);
+
+                    if (max < DataMatrix[i, j])
+                    {
+                        max = DataMatrix[i, j];
+                        MaxPositionX = i;
+                        MaxPositionY = j;
+                    }
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.WriteLine("{0} {1}", MaxPositionX + 1, MaxPositionY + 1);
+        }
+    }
+}
+
